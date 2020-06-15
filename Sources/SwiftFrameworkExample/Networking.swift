f1b0d27caa4a7196c28194946b2af66d8db732bd
Gabriel Palhares <gaapalhares@gmail.com>
2020-06-15 15:25:47 -0300

    Refactored the Networking API to include dependency injection for easier testing

--- Sources/SwiftFrameworkExample/Networking.swift
+++ Sources/SwiftFrameworkExample/Networking.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+}
+
 extension SwiftFramework {
     public class Networking {
         /// Responsable for handling  all networking calls
@@ -14,14 +27,17 @@ extension SwiftFramework {
         public class Manager {
             public init() {}
             
-            private let session = URLSession.shared
+            internal var session: NetworkSession = URLSession.shared
             
+            /// Calls to the live internet to retrieve Data from a specific location
+            /// - Parameters:
+            ///   - url: The location you wish to fetch data from
+            ///   - completion: Result object that represents the status of the request
             public func loadData(from url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) { (data, response, error) in
+                session.get(from: url) { (data, error) in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completion(result)
                 }
-                task.resume()
             }
         }
     }
--- Tests/SwiftFrameworkExampleTests/SwiftFrameworkNetworkingTests.swift
+++ Tests/SwiftFrameworkExampleTests/SwiftFrameworkNetworkingTests.swift
@@ -8,19 +8,33 @@
 import XCTest
 @testable import SwiftFrameworkExample
 
+class NetworkSessionMock: NetworkSession {
+    
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        completion(data, error)
+    }
+}
+
 final class SwiftFrameworkNetworkingTests: XCTestCase {
 
     func testLoadDataCall() {
         let manager = SwiftFramework.Networking.Manager()
+        
+        let session = NetworkSessionMock()
+        manager.session = session
+        
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
         let expectation = XCTestExpectation(description: "Called for data")
-        guard let url = URL(string: "https://raywenderlich.com") else {
-            return XCTFail("Could not create URL properly")
-        }
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let returnedData):
-                XCTAssertNil(returnedData, "Data is nil")
+                XCTAssertEqual(data, returnedData, "unexpected data received")
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error on forming Error object")
             }
